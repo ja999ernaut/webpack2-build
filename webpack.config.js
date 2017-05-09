c7494aed160ce4a77cf63133a14878fcfd0ec188
ja999ernaut <alexgerasimenko89@gmail.com>
2017-05-09 14:37:25 +0300

    Merge config

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,48 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const pug = require('./webpack/pug');
+const devserver = require('./webpack/devserver'); 
 
 const PATHS = {
 	source: path.join(__dirname, 'src'),
 	build: path.join(__dirname, 'build')
 };
 
-const common = {
-	entry: {
-        'index': PATHS.source + '/pages/index/index.js',
-        'blog': PATHS.source + '/pages/blog/blog.js'
-    },
-	output: {
-		path: PATHS.build,
-		filename: '[name].js'
+const common = merge([
+	{
+		entry: {
+	        'index': PATHS.source + '/pages/index/index.js',
+	        'blog': PATHS.source + '/pages/blog/blog.js'
+	    },
+		output: {
+			path: PATHS.build,
+			filename: '[name].js'
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+	            filename: 'index.html',
+	            chunks: ['index', 'common'],
+	            template: PATHS.source + '/pages/index/index.pug'
+	        }),
+	        new HtmlWebpackPlugin({
+	            filename: 'blog.html',
+	            chunks: ['blog', 'common'],
+	            template: PATHS.source + '/pages/blog/blog.pug'
+	        })
+		]
 	},
-	plugins: [
-		new HtmlWebpackPlugin({
-            filename: 'index.html',
-            chunks: ['index', 'common'],
-            template: PATHS.source + '/pages/index/index.pug'
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'blog.html',
-            chunks: ['blog', 'common'],
-            template: PATHS.source + '/pages/blog/blog.pug'
-        })
-	],
-	module: {
-	    rules: [
-	    	{
-		        test: /\.pug$/,
-		        loader: 'pug-loader',
-		        options: {
-		        	pretty: true
-		        }
-	      	}
-	    ]
-	}
-}; 
-
-const developmentConfig = {
-	devServer: {
-		port: 9000
-	}
-};
+	pug()
+]);
 
 module.exports = function(env) {
-	if (env === "production"){
+	if (env === 'production'){
 		return common;
 	}
-	if (env === "development"){
-		return Object.assign(
-			{},
+	if (env === 'development'){
+		return merge([
 			common,
-			developmentConfig
-		)
+			devserver()
+		])
 	}
 };
